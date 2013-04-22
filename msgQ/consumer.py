2af997cbd3e94247c0765d814461032c1e8a453f
hbc <bcxxxxxx@gmail.com>
2013-04-22 20:43:03 +0800

    refactored consumer

--- msgQ/consumer.py
+++ msgQ/consumer.py
@@ -6,26 +6,28 @@ msgQ.consumer
 
 
 class MsgConsumer(object):
-    def __init__(self, key, command=None):
+    def __init__(self, key, command=None, raw=False, args=None):
         self.key = key
         self.command = command
+        self._is_raw = raw
+        self._extra_args = args or ''
 
     def consume(self, payload):
         '''Adapt this method to handle the message'''
-        raise NotImplementedError
+        if self._is_raw:
+            return self._consume_raw(payload)
+        else:
+            raise NotImplementedError
 
-    def consume_raw(self, payload):
+    def _consume_raw(self, payload):
         '''Execute from string.'''
-        if not self._is_raw and self.command:
+        if not self.command:
             raise AttributeError
 
         import sh
-        c = sh.Command(self.command)(payload)
+        c = sh.Command(self.command)(self._extra_args, payload)
         c.wait()
         return c.exit_code
 
-    @staticmethod
-    def from_raw(key, command):
-        c = MsgConsumer(key, command)
-        c._is_raw = True
-        return c
+    def __str__(self):
+        return '<Consumer %s>' % (self.key)
